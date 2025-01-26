e6c684ed6b542dacc70abe48aca4294bdfe4748d
pao <psdelarosa@gmail.com>
2025-01-26 09:39:22 +0000

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This is test application</p>
         <a
           className="App-link"
           href="https://reactjs.org"
